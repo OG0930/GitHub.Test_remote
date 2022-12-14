5733a7072dd9ae6bea79858ea6f95566bf18fd24
OGXG <oji0930@gmail.com>
2022-12-15 01:53:37 +0800

    Initial Commit

---
+++ GitHub.Test/GitHub_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestApp.swift
+//  GitHub.Test
+//
+//  Created by OG XG on 2022.12.15.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
